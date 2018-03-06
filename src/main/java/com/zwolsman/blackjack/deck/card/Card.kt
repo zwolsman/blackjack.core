3052cc989ba820ee329291a0c221f39efa4d9c4d
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-07 00:38:09 +0100

    Updated card factory function to ignore all white space

--- src/main/java/com/zwolsman/blackjack/deck/card/Card.kt
+++ src/main/java/com/zwolsman/blackjack/deck/card/Card.kt
@@ -3,8 +3,11 @@ package com.zwolsman.blackjack.deck.card
 data class Card(val suit: Suit, val rank: Rank) {
     companion object {
         val BLANK = Card(Suit.NONE, Rank.NONE)
-        fun fromString(str: String) = Card(Suit.values().first { it.icon == str.substringBefore(" ") }, Rank.values().first { it.icon == str.substringAfter(" ") })
-
+        fun fromString(str: String) : Card{
+            val icon = str.substring(0..1).trim()
+            val rank = str.substring(1 until str.length).trim()
+            return Card(Suit.values().first { it.icon == icon }, Rank.values().first { it.icon == rank })
+        }
     }
     override fun toString() = "Card($icon)"
 
