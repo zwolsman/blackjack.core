9d2dd1e6596bfab9a9c9a2cb7d4a1e10102ff3bf
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-06 11:02:25 +0100

    short function for toString

--- src/main/java/com/zwolsman/blackjack/deck/card/Card.kt
+++ src/main/java/com/zwolsman/blackjack/deck/card/Card.kt
@@ -1,9 +1,7 @@
 package com.zwolsman.blackjack.deck.card
 
 data class Card(val suit: Suit, val rank: Rank) {
-    override fun toString(): String {
-        return "Card($icon)"
-    }
+    override fun toString() = "Card($icon)"
 
     val icon:String
         get() = "${suit.icon} ${rank.icon}"
