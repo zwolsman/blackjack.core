b1445fba70ff32da675789c7c5ea8bf974bb2136
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-08 10:51:41 +0100

    Rewrote DealerTest Using Spek now

--- src/test/kotlin/DealerTest.kt
+++ src/test/kotlin/DealerTest.kt
@@ -1,5 +1,10 @@
 import com.zwolsman.blackjack.core.Game
 import com.zwolsman.blackjack.core.game.Option
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -7,41 +12,37 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
 import java.util.stream.Stream
 
-
-class DealerTest {
-    companion object {
-        val rng = Random(0)
-        @JvmStatic
-        fun createSeeds() : Stream<Long> =
-            Stream.iterate(0) {
-                it + 1
-            }.limit(10).map {
-                rng.nextLong()
+class DealerTest : Spek({
+    context("static seed") {
+        given("a game with seed 0") {
+            with("seed 0") {
+                val game = Game(0)
+                on("player stand") {
+                    game.player[0].playOption(Option.STAND)
+                    it("should finish the game and have 20 points") {
+                        val expectedCards = "♣ K, ♥ Q".toCards()
+                        val expectedPoints = 20
+                        assertIterableEquals(expectedCards, game.dealer.cards)
+                        assertEquals(listOf(expectedPoints), game.dealer.points)
+                        assertTrue(game.isFinished)
+                    }
+                }
             }
         }
-
-    @Test
-    fun `dealers hits with 0 seed`() {
-        val game = Game()
-        //Stand on hand so the dealer plays
-        game.player[0].playOption(Option.STAND)
-
-        val expectedCards = "♣ K, ♥ Q".toCards()
-        val expectedPoints = 20
-        assertIterableEquals(expectedCards, game.dealer.cards)
-        assertEquals(listOf(expectedPoints), game.dealer.points)
     }
-
-    @ParameterizedTest
-    @MethodSource("createSeeds")
-    fun `dealers hits random seed`(seed:Long) {
-        val game = Game(seed)
-        val before = game.dealer.points
-        game.player[0].playOption(Option.STAND)
-        val after = game.dealer.points
-
-        assertNotSame(after, before)
-        assertTrue(game.dealer.points.last() >= 17)
-        assertTrue(game.isFinished)
+    context("random seed") {
+        val seed: Long = 10
+        given("a game with seed $seed") {
+            val game = Game(seed)
+            on("player stand") {
+                game.player[0].playOption(Option.STAND)
+                it("should finish the game") {
+                    assertTrue(game.isFinished)
+                }
+                it("shouldn't have a blank card") {
+                    assertFalse(game.dealer.hasBlank)
+                }
+            }
+        }
     }
-}
+})
