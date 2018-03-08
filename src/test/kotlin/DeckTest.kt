5c03dfddba7dee3e92e56c68f846d92ff4a289dd
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-08 12:40:32 +0100

    Rewrote DeckTest

--- src/test/kotlin/DeckTest.kt
+++ src/test/kotlin/DeckTest.kt
@@ -1,54 +1,51 @@
 import com.zwolsman.blackjack.core.deck.Deck
 import com.zwolsman.blackjack.core.deck.card.Card
-import com.zwolsman.blackjack.core.deck.card.Rank
-import com.zwolsman.blackjack.core.deck.card.Suit
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 
-class DeckTest {
-    @Test
-    fun `1 deck is 52 cards`() {
-        val deck = Deck(1)
-        assertEquals(52, deck.cards.size)
+class DeckTest : Spek({
+
+    given("a deck") {
+        context("of size 1") {
+            val deck = Deck(1)
+            it("should have 52 cards") {
+                assertEquals(52, deck.cards.size)
+            }
+        }
+
+        context("of size 2") {
+            val deck = Deck(2)
+            it("should have 104 cards") {
+                assertEquals(104, deck.cards.size)
+            }
+        }
+
+        it("should be iterable") {
+            val deck = Deck(1)
+            assertTrue(deck is Iterable<Card>)
+        }
+
+    }
+    given("a shuffled deck") {
+        it("should be shuffled") {
+            val deck = Deck(1)
+            assertNotEquals(deck, Deck.FULL_DECK)
+        }
     }
 
-    @Test
-    fun `2 decks is 104 cards`() {
-        val deck = Deck(2)
-        assertEquals(104, deck.cards.size)
-    }
+    given("a static deck") {
+        val deck = Deck.FULL_DECK
 
-
-    @Test
-    fun `seed sorts deck`() {
-        val deck = Deck(1)
-        assertEquals(Card(Suit.SPADES, Rank.TEN), deck.cards[0])
-        assertEquals(Card(Suit.CLUBS, Rank.KING), deck.cards[1])
+        it("should not be shuffled") {
+            val expected = (
+                    "♦ 2, ♦ 3, ♦ 4, ♦ 5, ♦ 6, ♦ 7, ♦ 8, ♦ 9, ♦ 10, ♦ J, ♦ Q, ♦ K, ♦ A, " +
+                            "♣ 2, ♣ 3, ♣ 4, ♣ 5, ♣ 6, ♣ 7, ♣ 8, ♣ 9, ♣ 10, ♣ J, ♣ Q, ♣ K, ♣ A, " +
+                            "♥ 2, ♥ 3, ♥ 4, ♥ 5, ♥ 6, ♥ 7, ♥ 8, ♥ 9, ♥ 10, ♥ J, ♥ Q, ♥ K, ♥ A, " +
+                            "♠ 2, ♠ 3, ♠ 4, ♠ 5, ♠ 6, ♠ 7, ♠ 8, ♠ 9, ♠ 10, ♠ J, ♠ Q, ♠ K, ♠ A").toCards()
+            assertIterableEquals(expected, deck)
+        }
     }
-
-    @Test
-    fun `deck is iterable`() {
-        val deck = Deck(1)
-        assertTrue(deck.iterator().hasNext())
-        assertIterableEquals(deck.cards, deck)
-    }
-
-    @Test
-    fun `deck index get works`() {
-        val deck = Deck(1)
-        val expected = Card(Suit.SPADES, Rank.TEN)
-        assertEquals(expected, deck.get(0))
-        assertEquals(expected, deck[0])
-    }
-    @Test
-    fun `full deck = 52 cards`() {
-        val cards = Deck.FULL_DECK
-        val expected = (
-                "♦ 2, ♦ 3, ♦ 4, ♦ 5, ♦ 6, ♦ 7, ♦ 8, ♦ 9, ♦ 10, ♦ J, ♦ Q, ♦ K, ♦ A, " +
-                "♣ 2, ♣ 3, ♣ 4, ♣ 5, ♣ 6, ♣ 7, ♣ 8, ♣ 9, ♣ 10, ♣ J, ♣ Q, ♣ K, ♣ A, " +
-                "♥ 2, ♥ 3, ♥ 4, ♥ 5, ♥ 6, ♥ 7, ♥ 8, ♥ 9, ♥ 10, ♥ J, ♥ Q, ♥ K, ♥ A, " +
-                "♠ 2, ♠ 3, ♠ 4, ♠ 5, ♠ 6, ♠ 7, ♠ 8, ♠ 9, ♠ 10, ♠ J, ♠ Q, ♠ K, ♠ A").toCards()
-        assertEquals(expected, cards.toCollection(ArrayList()))
-        assertEquals(52, cards.size)
-    }
-}
+})
