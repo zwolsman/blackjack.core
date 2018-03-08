4aaa30e2488aae09a8adef51eec3cadf136be1d1
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-08 09:43:29 +0100

    Rewrote init function of game

--- src/main/kotlin/com/zwolsman/blackjack/core/Game.kt
+++ src/main/kotlin/com/zwolsman/blackjack/core/Game.kt
@@ -6,31 +6,44 @@ import com.zwolsman.blackjack.core.game.Hand
 import com.zwolsman.blackjack.core.game.Option
 import com.zwolsman.blackjack.core.game.Status
 
-class Game(seed:Long = 0) {
-    constructor(seed:Long, vararg handCount:Int) : this(seed) {
+class Game(seed: Long = 0) {
+    //Reconstruct game from seed
+    constructor(seed: Long, vararg handCount: Int) : this(seed) {
 
-        if(handCount.size > 1)
+        if (handCount.size > 1)
             TODO("Reconstruct game with multiple hands")
 
 
         for ((index, h) in handCount.withIndex())
-            for(i in 0 until h - 2) {
+            for (i in 0 until h - 2) {
                 player[index].playOption(Option.HIT)
             }
     }
 
     val deck = Deck(1, seed)
-    val dealer = Hand(deck.cards[1], Card.BLANK)
-    val player = mutableListOf(Hand(deck.cards[0], deck.cards[2]))
+    val dealer = Hand()
+    val player = mutableListOf(Hand())
 
-    private val cardIndex:Int
-            get() = dealer.cards.size + player.map { it.cards.size }.sum()
+    init {
+        player.forEach { it.playOption = ::playOption }
+        dealer.playOption = ::playOption
+        fillHands(true)
+    }
+
+
+
+    private val cardIndex: Int
+        get() = dealer.cards.size + player.sumBy { it.cards.size }
 
     val nextCard: Card
         get() = deck[cardIndex]
 
+    val isFinished: Boolean
+        get() =
+            !dealer.status.canPlay && player.none { it.status.canPlay }
+
     private fun playOption(hand: Hand, option: Option) {
-        when(option) {
+        when (option) {
             Option.HIT -> {
                 hand.addCard(nextCard)
             }
@@ -47,18 +60,28 @@ class Game(seed:Long = 0) {
             Option.DOUBLE -> TODO()
             Option.INSURANCE -> TODO()
         }
-        if(player.none { it.status.canPlay })
+        if (player.none { it.status.canPlay })
             playDealer()
     }
 
-    private fun fillHands() {
-        for(hand in player)
-            if(hand.cards.size < 2)
-                hand.addCard(nextCard)
+    private fun fillHands(setup: Boolean = false) {
+        while (dealer.cards.size < 2 || player.any { it.cards.size < 2 }) {
+            for (hand in player) {
+                if (hand.cards.size < 2)
+                    hand.addCard(nextCard)
+            }
+            if (!setup || dealer.cards.size >= 2)
+                continue
+
+            when (dealer.cards.size) {
+                1 -> dealer.addCard(Card.BLANK)
+                else -> dealer.addCard(nextCard)
+            }
+        }
     }
 
 
-    private fun playDealer(stopsAtSoft17:Boolean = true) {
+    private fun playDealer(stopsAtSoft17: Boolean = true) {
         while (true) {
             val points = if (stopsAtSoft17) {
                 dealer.points.last()
@@ -66,13 +89,13 @@ class Game(seed:Long = 0) {
                 dealer.points.first()
             }
 
-            if(dealer.hasBlank) {
+            if (dealer.hasBlank) {
                 dealer.cards[1] = deck[3]
                 continue
             }
 
             if (points >= 17) {
-                if(points <= 21)
+                if (points <= 21)
                     dealer.status = Status.FINISHED
                 break
             }
@@ -81,13 +104,5 @@ class Game(seed:Long = 0) {
         }
     }
 
-    init {
-        player.forEach { it.playOption = ::playOption }
-        dealer.playOption = ::playOption
-    }
-
-    val isFinished: Boolean
-        get() =
-            !dealer.status.canPlay && player.none { it.status.canPlay }
 }
 
