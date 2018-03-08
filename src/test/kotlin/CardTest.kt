e7baa707078b70817680f7da5aa6130b16226097
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-08 10:11:33 +0100

    Rewrote CardTests Using Spek now

--- src/test/kotlin/CardTest.kt
+++ src/test/kotlin/CardTest.kt
@@ -1,22 +1,37 @@
 import com.zwolsman.blackjack.core.deck.card.Card
 import com.zwolsman.blackjack.core.deck.card.Rank
 import com.zwolsman.blackjack.core.deck.card.Suit
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class CardTest {
-    @Test
-    fun `icon matches definition`() {
-        val spade10 = Card(Suit.SPADES, Rank.TEN)
-        val expectedSpade10 = "♠ 10"
-        assertEquals(expectedSpade10, spade10.icon)
+class CardTest : Spek({
+    given("a card") {
+        on("creation of ten of spades") {
+            it("should have the icon ♠ 10") {
+                val expected = "♠ 10"
+                val spade10 = Card(Suit.SPADES, Rank.TEN)
+                assertEquals(expected, spade10.icon)
+            }
+        }
+        on("creation of clubs queen") {
+            it("should have the icon ♣ Q") {
+                val expected = "♣ Q"
+                val clubsQueen = Card(Suit.CLUBS, Rank.QUEEN)
+                assertEquals(expected, clubsQueen.icon)
+            }
+        }
+        on("creation of a diamonds king") {
+            it("should have the icon ♦ K") {
+                val expected = "♦ K"
+                val diamondKing = Card(Suit.DIAMONDS, Rank.KING)
+                assertEquals(expected, diamondKing.icon)
+            }
+        }
 
-        val diamondKing = Card(Suit.DIAMONDS, Rank.KING)
-        val expectedDiamondKing = "♦ K"
-        assertEquals(expectedDiamondKing, diamondKing.icon)
-
-        val clubsQueen = Card(Suit.CLUBS, Rank.QUEEN)
-        val expectedClubsQueen = "♣ Q"
-        assertEquals(expectedClubsQueen, clubsQueen.icon)
     }
-}
+})
